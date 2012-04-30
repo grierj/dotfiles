d9b43669d123f3912538593ed678f7c981e85d64
Grier Johnson <grierj@gmail.com>
2012-04-30 14:02:26 -0700

    Removing prompt, switching force to mean "don't backup"

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,17 +18,14 @@ function BackUp() {
   done
 }
 
-BackUp
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    BackUp
+	doIt
 fi
 unset doIt
+unset BackUp
 source ~/.bash_profile
 
